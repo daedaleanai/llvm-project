1f1fdfd6fef44a7eee058d976a6e2f57ca4fd811
Nikolai Filchenko <nf@daedalean.ai>
2022-03-03 13:52:32 +0100

    BUILD.go for integration with DBT

---
+++ BUILD.go
@@ -0,0 +1,15 @@
+package llvm
+
+var CLang       = in("bin/clang")
+var ClangPp     = in("bin/clang++")
+var Lld         = in("bin/lld")
+var Ar          = in("bin/llvm-ar")
+var As          = in("bin/llvm-as")
+var Objdump     = in("bin/llvm-objdump")
+var Objcopy     = in("bin/llvm-objcopy")
+var ClangTidy   = in("bin/clang-tidy")
+var ClangFormat = in("bin/clang-format")
+var Cov         = in("bin/llvm-cov")
+
+
+
