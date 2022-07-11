123779871d790bdeb00f2593dfe5f23e18fe4195
Javier Alvarez Garcia <ja@daedalean.ai>
2022-07-11 15:16:17 +0200

    Fix daedalean-structs-and-classes check

--- clang-tools-extra/clang-tidy/daedalean/StructsAndClassesCheck.cpp
+++ clang-tools-extra/clang-tidy/daedalean/StructsAndClassesCheck.cpp
@@ -16,6 +16,51 @@ namespace clang {
 namespace tidy {
 namespace daedalean {
 
+StructsAndClassesCheck::ShouldBeAClass
+StructsAndClassesCheck::shouldBeAClass(const CXXRecordDecl *record) {
+  if (record->hasUserDeclaredCopyConstructor()) {
+    return ShouldBeAClass::YesHasUserDeclaredCopyConstructor;
+  }
+
+  if (record->hasUserDeclaredMoveConstructor()) {
+    return ShouldBeAClass::YesHasUserDeclaredMoveConstructor;
+  }
+
+  if (record->hasUserDeclaredCopyAssignment()) {
+    return ShouldBeAClass::YesHasUserDeclaredCopyAssignment;
+  }
+
+  if (record->hasUserDeclaredMoveAssignment()) {
+    return ShouldBeAClass::YesHasUserDeclaredCopyAssignment;
+  }
+
+  if (record->hasUserDeclaredDestructor()) {
+    return ShouldBeAClass::YesHasUserDeclaredDestructor;
+  }
+
+  if (!record->isAggregate()) {
+    return ShouldBeAClass::YesNotAnAggregate;
+  }
+
+  for (const CXXMethodDecl *method : record->methods()) {
+    const bool isConstructor = llvm::isa<CXXConstructorDecl>(method);
+    if (isConstructor) {
+      // Constructors are ignored
+      continue;
+    }
+
+    if (method->isStatic()) {
+      continue;
+    }
+
+    if (!method->isConst()) {
+      return ShouldBeAClass::YesHasNonConstMethod;
+    }
+  }
+
+  return ShouldBeAClass::No;
+}
+
 void StructsAndClassesCheck::registerMatchers(MatchFinder *Finder) {
   Finder->addMatcher(cxxRecordDecl(hasDefinition()).bind("x"), this);
 }
@@ -28,29 +73,73 @@ void StructsAndClassesCheck::check(const MatchFinder::MatchResult &Result) {
     return;
   }
 
-  if (MatchedDecl->isPOD()) {
+  if (const ShouldBeAClass shouldBeClass = shouldBeAClass(MatchedDecl);
+      shouldBeClass != ShouldBeAClass::No) {
+    if (MatchedDecl->isStruct()) {
+      switch (shouldBeClass) {
+      case ShouldBeAClass::YesNotAnAggregate:
+        diag(MatchedDecl->getBeginLoc(),
+             "Non-Aggregate type %0 must be declared as a class")
+            << MatchedDecl;
+        break;
+      case ShouldBeAClass::YesHasUserDeclaredCopyConstructor:
+        diag(MatchedDecl->getBeginLoc(),
+             "Type %0 with a user-provided copy constructor must be declared "
+             "as a class")
+            << MatchedDecl;
+        break;
+      case ShouldBeAClass::YesHasUserDeclaredMoveConstructor:
+        diag(MatchedDecl->getBeginLoc(),
+             "Type %0 with a user-provided move constructor must be declared "
+             "as a class")
+            << MatchedDecl;
+        break;
+      case ShouldBeAClass::YesHasUserDeclaredCopyAssignment:
+        diag(MatchedDecl->getBeginLoc(),
+             "Type %0 with a user-provided move copy assignment operator must "
+             "be declared as a class")
+            << MatchedDecl;
+        break;
+      case ShouldBeAClass::YesHasUserDeclaredMoveAssignment:
+        diag(MatchedDecl->getBeginLoc(),
+             "Type %0 with a a user-provided move move assignment operator "
+             "must be declared as a class")
+            << MatchedDecl;
+        break;
+      case ShouldBeAClass::YesHasUserDeclaredDestructor:
+        diag(MatchedDecl->getBeginLoc(),
+             "Type %0 with a user-provided destructor must be declared as a "
+             "class")
+            << MatchedDecl;
+        break;
+      case ShouldBeAClass::YesHasNonConstMethod:
+        diag(MatchedDecl->getBeginLoc(),
+             "Type %0 with non-const methods must be declared as a class")
+            << MatchedDecl;
+        break;
+      default:
+        break;
+      }
+      diag(MatchedDecl->getBeginLoc(), "use class", DiagnosticIDs::Note)
+          << FixItHint::CreateInsertion(MatchedDecl->getBeginLoc(), "class");
+    }
+  } else {
     if (!MatchedDecl->isStruct()) {
-      diag(MatchedDecl->getBeginLoc(), "POD type %0 must be declared as struct")
+      diag(MatchedDecl->getBeginLoc(), "Type %0 must be declared as a struct")
           << MatchedDecl;
       diag(MatchedDecl->getBeginLoc(), "use struct", DiagnosticIDs::Note)
           << FixItHint::CreateReplacement(MatchedDecl->getBeginLoc(), "struct");
     }
-  } else {
-    if (!MatchedDecl->isClass()) {
-      diag(MatchedDecl->getBeginLoc(), "Non-POD type %0 must be declared as class")
-          << MatchedDecl;
-      diag(MatchedDecl->getBeginLoc(), "use class", DiagnosticIDs::Note)
-          << FixItHint::CreateReplacement(MatchedDecl->getBeginLoc(), "class");
-    }
   }
 
   if (MatchedDecl->isClass()) {
-    for (const auto * field: MatchedDecl->fields()) {
+    for (const auto *field : MatchedDecl->fields()) {
       if (field->getType().isConstQualified()) {
         continue;
       }
       if (field->getAccess() != AS_private && field->getAccess() != AS_none) {
-        diag(field->getBeginLoc(), "All non-const data members of class MUST be private")
+        diag(field->getBeginLoc(),
+             "All non-const data members of class MUST be private")
             << MatchedDecl;
         diag(field->getBeginLoc(), "Make field private", DiagnosticIDs::Note)
             << FixItHint::CreateInsertion(field->getBeginLoc(), "private:");
