03bc5bbb56c79942b20ec0a3c4a358e686f6fadf
Javier Alvarez Garcia <ja@daedalean.ai>
2022-07-11 10:41:35 +0200

    Fix daedalean-friend-declarations check
    Use the right argument to check if the friend is an overloaded
    operator. Remove `#if 0` that prevented the test from passing.
    
    Tested with:
    ./build/bin/llvm-lit -v clang-tools-extra/test/clang-tidy/checkers/daedalean-friend-declarations.cpp

--- clang-tools-extra/test/clang-tidy/checkers/daedalean-friend-declarations.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/daedalean-friend-declarations.cpp
@@ -1,20 +1,21 @@
 // RUN: %check_clang_tidy %s daedalean-friend-declarations %t
-#if 0
+
 class S {};
+
+class B {};
 
 class A {
   friend class B;
   // CHECK-MESSAGES: :[[@LINE-1]]:10: warning: Friend declaration must not be used [daedalean-friend-declarations]
   friend void foo();
   // CHECK-MESSAGES: :[[@LINE-1]]:15: warning: Friend declaration must not be used [daedalean-friend-declarations]
-  friend S& operator << (S&, const A&);
+  friend S &operator<<(S &, const A &);
 };
 
-#endif
-template<typename T>
+template <typename T>
 class C {
 
-  template<typename U>
+  template <typename U>
   friend class C;
 };
 
