7b0efe99fb62fea4ffd4de1b510013b4ac950db8
Javier Alvarez Garcia <ja@daedalean.ai>
2022-07-20 09:29:28 +0200

    Make sure daedalean-operator-assignment can be supressed in macro

--- clang-tools-extra/test/clang-tidy/checkers/daedalean-assignment-operators.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/daedalean-assignment-operators.cpp
@@ -1,25 +1,35 @@
 // RUN: %check_clang_tidy %s daedalean-assignment-operators %t
 
+#define CLASS(name) class name final
+#define CLASS_NOLINT(name) class name final /* NOLINT(daedalean-assignment-operators) */
+
+CLASS(MyClass){
+    // CHECK-MESSAGES: :[[@LINE-1]]:1: warning: Non-abstract class 'MyClass' must implement copy-assignment operator [daedalean-assignment-operators]
+    // CHECK-MESSAGES: :[[@LINE-2]]:1: warning: Non-abstract class 'MyClass' must implement move-assignment operator [daedalean-assignment-operators]
+};
+
+CLASS_NOLINT(MyClassWithoutLints){};
+
 class C1 {
-  // CHECK-MESSAGES: :[[@LINE-1]]:7: warning: Non-abstract class 'C1' must implement copy-assignment operator [daedalean-assignment-operators]
-  // CHECK-MESSAGES: :[[@LINE-2]]:7: warning: Non-abstract class 'C1' must implement move-assignment operator [daedalean-assignment-operators]
+  // CHECK-MESSAGES: :[[@LINE-1]]:1: warning: Non-abstract class 'C1' must implement copy-assignment operator [daedalean-assignment-operators]
+  // CHECK-MESSAGES: :[[@LINE-2]]:1: warning: Non-abstract class 'C1' must implement move-assignment operator [daedalean-assignment-operators]
 public:
 };
 
 class C2 {
-  // CHECK-MESSAGES: :[[@LINE-1]]:7: warning: Non-abstract class 'C2' must implement move-assignment operator [daedalean-assignment-operators]
+  // CHECK-MESSAGES: :[[@LINE-1]]:1: warning: Non-abstract class 'C2' must implement move-assignment operator [daedalean-assignment-operators]
 public:
   C2 &operator=(const C2 &);
 };
 
 class C3 {
-  // CHECK-MESSAGES: :[[@LINE-1]]:7: warning: Non-abstract class 'C3' must implement copy-assignment operator [daedalean-assignment-operators]
+  // CHECK-MESSAGES: :[[@LINE-1]]:1: warning: Non-abstract class 'C3' must implement copy-assignment operator [daedalean-assignment-operators]
 public:
   C3 &operator=(C3 &&);
 };
 
 class C4 {
-  // CHECK-MESSAGES: :[[@LINE-1]]:7: warning: Non-abstract class 'C4' must implement move-assignment operator [daedalean-assignment-operators]
+  // CHECK-MESSAGES: :[[@LINE-1]]:1: warning: Non-abstract class 'C4' must implement move-assignment operator [daedalean-assignment-operators]
 public:
   C4 &operator=(C4 &);
 };
