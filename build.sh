2eafb59f1c0a4bbecbd3886e1357174237531978
Javier Alvarez Garcia <ja@daedalean.ai>
2022-08-03 14:18:10 +0200

    Create release script

--- build.sh
+++ build.sh
@@ -1,22 +1,16 @@
-#!/bin/bash
+#!/bin/bash -e
+
 mkdir -p DIST/llvm
 
-cmake -S llvm -B build -G "Unix Makefiles" \
+cmake -S llvm -B build -G Ninja \
     -DLLVM_ENABLE_PROJECTS="clang;lld;clang-tools-extra" \
     -DLLVM_BUILD_RUNTIMES=OFF \
     -DCMAKE_INSTALL_PREFIX=`pwd`"/DIST/llvm" \
     -DCMAKE_BUILD_TYPE="Release" \
     -DLLVM_TARGETS_TO_BUILD=X86 \
     -DCMAKE_CXX_STANDARD=20
- 
-    #-DLIBCLANG_BUILD_STATIC=ON \
-    #-DLLVM_STATIC_LINK_CXX_STDLIB=ON \
-    #-DBUILD_SHARED_LIBS=OFF
-    #-DLLVM_BUILD_STATIC=ON \
-    #-DLLVM_ENABLE_RUNTIMES="compiler-rt" \
 
-cd build
-make -j5
-make install
-cd ..
+cmake --build build -- -j5
+cmake --build build -- install
+
 cp BUILD.go DIST/llvm
